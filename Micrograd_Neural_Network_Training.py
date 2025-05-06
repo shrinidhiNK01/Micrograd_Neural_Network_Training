04bc93904f9e7b7c73248e1f993c168f04af1180
shrinidhi <kulkarnishrinidhi0001@gmail.com>
2025-05-06 14:51:42 +0530

    update Micrograd_Neural_Network_Training.py

---
+++ Micrograd_Neural_Network_Training.py
@@ -0,0 +1,5 @@
+# micrograd is a autograd engine(automatic gradient)
+# it helps in backpropogation(mathematical core):-algorithm to train neural networks that effeciently evaluate the gradient that minimize loss function(errors) b/w predicted and actual output and increase accuracy of neural networks and adjust weights
+# neural networks are just mathematical expressions
+# backpropogation:-error propogated backward using chain rule(calculus)
+# Micrograd is a scaler valued autograd engine
